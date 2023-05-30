d79650077c56bc5d9e8d02f718afc196352c2c38
Amirhossein-mousavi <ah.mousavia06@gmail.com>
2023-05-31 00:26:38 +0430

    create SetupNavGraph

---
+++ app/src/main/java/ir/reversedev/documentbag/navigation/NavGraph.kt
@@ -0,0 +1,12 @@
+package ir.reversedev.documentbag.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+
+@Composable
+fun SetupNavGraph(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = Screen.Splash.route) {
+
+    }
+}
