0f7c7b85e6f4ecb99ca518194ae40192a50b260c
Amirhossein-mousavi <ah.mousavia06@gmail.com>
2023-05-31 00:16:59 +0430

    add navHostController into MainActivity

--- app/src/main/java/ir/reversedev/documentbag/MainActivity.kt
+++ app/src/main/java/ir/reversedev/documentbag/MainActivity.kt
@@ -3,26 +3,29 @@ package ir.reversedev.documentbag
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import ir.reversedev.documentbag.ui.theme.DocumentBagTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController : NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DocumentBagTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                navController = rememberNavController()
+                Scaffold (
+                    bottomBar = {
+                        //TODO BOTTOM BAR
+                    }
+                        ) {
+
+
                 }
             }
         }
