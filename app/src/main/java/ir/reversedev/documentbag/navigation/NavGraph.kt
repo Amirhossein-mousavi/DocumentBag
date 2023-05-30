9f09a41392f1369f0533947f9ff77444e2ecdf6f
Amirhossein-mousavi <ah.mousavia06@gmail.com>
2023-05-31 00:29:02 +0430

    create screens composable in SetupNavGraph

--- app/src/main/java/ir/reversedev/documentbag/navigation/NavGraph.kt
+++ app/src/main/java/ir/reversedev/documentbag/navigation/NavGraph.kt
@@ -3,10 +3,20 @@ package ir.reversedev.documentbag.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 
 @Composable
 fun SetupNavGraph(navController: NavHostController) {
     NavHost(navController = navController, startDestination = Screen.Splash.route) {
 
+        composable(Screen.Splash.route) {}
+
+        composable(Screen.Home.route) {}
+
+        composable(Screen.Note.route) {}
+
+        composable(Screen.Setting.route) {}
+
+        composable(Screen.Card.route) {}
     }
 }
