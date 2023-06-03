c9785854449e781aeaa9e3080f65ee3cac11c4b0
amirhossein <musavia06@gmail.com>
2023-06-03 18:12:35 +0330

    add splash screen to SetupNavGraph

--- app/src/main/java/ir/reversedev/documentbag/navigation/NavGraph.kt
+++ app/src/main/java/ir/reversedev/documentbag/navigation/NavGraph.kt
@@ -4,12 +4,15 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import ir.reversedev.documentbag.ui.screen.SplashScreen
 
 @Composable
 fun SetupNavGraph(navController: NavHostController) {
     NavHost(navController = navController, startDestination = Screen.Splash.route) {
 
-        composable(Screen.Splash.route) {}
+        composable(Screen.Splash.route) {
+            SplashScreen(navController = navController)
+        }
 
         composable(Screen.Home.route) {}
 
