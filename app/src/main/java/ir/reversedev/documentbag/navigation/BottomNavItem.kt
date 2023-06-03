03e3c25bcff10cebede6d26f6b1170d185e67cd2
amirhossein <musavia06@gmail.com>
2023-06-03 19:36:08 +0330

    create BottomNavItem

---
+++ app/src/main/java/ir/reversedev/documentbag/navigation/BottomNavItem.kt
@@ -0,0 +1,10 @@
+package ir.reversedev.documentbag.navigation
+
+import androidx.compose.ui.graphics.painter.Painter
+
+data class BottomNavItem(
+    val name: String,
+    val route: String,
+    val selectedIcon: Painter,
+    val deSelectedIcon: Painter,
+)
