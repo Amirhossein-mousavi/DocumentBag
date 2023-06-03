d80a8eb2e279e77b040a3233d0cb076e5733097f
amirhossein <musavia06@gmail.com>
2023-06-03 18:03:44 +0330

    create primaryColors function

--- app/src/main/java/ir/reversedev/documentbag/ui/theme/Color.kt
+++ app/src/main/java/ir/reversedev/documentbag/ui/theme/Color.kt
@@ -1,5 +1,7 @@
 package ir.reversedev.documentbag.ui.theme
 
+import android.provider.CalendarContract.Colors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +11,14 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val Colors.primaryColor : Color
+@Composable
+get() = Color(0xFF03A9F4)
+val Colors.primaryVariantColor : Color
+    @Composable
+    get() = Color(0xFF0277BD)
+val Colors.onPrimaryColor : Color
+    @Composable
+    get() = Color(0xFFFFFFFF)
