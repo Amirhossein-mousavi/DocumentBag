2a7b027cd44ed0f332ab8c4040f01c6b21b79a9d
amirhossein <musavia06@gmail.com>
2023-06-03 18:11:26 +0330

    add image to SplashScreen

--- app/src/main/java/ir/reversedev/documentbag/ui/screen/SplashScreen.kt
+++ app/src/main/java/ir/reversedev/documentbag/ui/screen/SplashScreen.kt
@@ -1,9 +1,44 @@
 package ir.reversedev.documentbag.ui.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import ir.reversedev.documentbag.R
+import ir.reversedev.documentbag.navigation.Screen
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
+    Splash()
+    LaunchedEffect(true) {
+        delay(2000)
+        navController.navigate(Screen.Home.route)
+    }
+}
 
+@Composable
+fun Splash() {
+    Box(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primary)
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    )
+    {
+        Image(
+            painter = painterResource(id = R.drawable.splash_image),
+            contentDescription = null,
+            modifier = Modifier.size(250.dp)
+        )
+    }
 }
