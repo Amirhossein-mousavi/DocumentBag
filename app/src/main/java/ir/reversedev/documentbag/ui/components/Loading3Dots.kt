0eabb3ca1cf05ed4adeda2dd443cb6645516ab9a
amirhossein <musavia06@gmail.com>
2023-06-03 18:33:01 +0330

    create Loading3dots components

--- app/src/main/java/ir/reversedev/documentbag/ui/components/Loading3Dots.kt
+++ app/src/main/java/ir/reversedev/documentbag/ui/components/Loading3Dots.kt
@@ -1,7 +1,16 @@
 package ir.reversedev.documentbag.ui.components
 
+<<<<<<< HEAD
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+=======
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+>>>>>>> 0e67d8f (create Loading3dots components)
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
@@ -9,7 +18,11 @@ import com.airbnb.lottie.compose.rememberLottieComposition
 import ir.reversedev.documentbag.R
 
 @Composable
+<<<<<<< HEAD
 fun Loading3dots(isDark: Boolean) {
+=======
+fun Loading3dots(isDark: Boolean, size: Dp = 100.dp) {
+>>>>>>> 0e67d8f (create Loading3dots components)
     val composition by rememberLottieComposition(
         spec =
         if (isDark)
@@ -17,7 +30,15 @@ fun Loading3dots(isDark: Boolean) {
         else
             LottieCompositionSpec.RawRes(R.raw.loading3dotslight)
     )
+<<<<<<< HEAD
     LottieAnimation(composition = composition, iterations = LottieConstants.IterateForever)
+=======
+    LottieAnimation(
+        composition = composition,
+        iterations = LottieConstants.IterateForever,
+        modifier = Modifier.size(size = size)
+    )
+>>>>>>> 0e67d8f (create Loading3dots components)
 
 
 }
