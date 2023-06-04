8beab3f3448b0de3378fc1715d1e401213df8e9d
amirhossein <musavia06@gmail.com>
2023-06-04 14:54:57 +0330

    create DataStoreRepositoryImpl

---
+++ app/src/main/java/ir/reversedev/documentbag/data/datastore/DataStoreRepositoryImpl.kt
@@ -0,0 +1,54 @@
+package ir.reversedev.documentbag.data.datastore
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import ir.reversedev.documentbag.utils.Constants.DATA_STORE_NAME
+import kotlinx.coroutines.flow.first
+import javax.inject.Inject
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = DATA_STORE_NAME)
+
+class DataStoreRepositoryImpl @Inject constructor(
+    private val context: Context
+) : DataStoreRepository {
+    override suspend fun putString(key: String, value: String) {
+        val preferencesKey = stringPreferencesKey(key)
+        context.dataStore.edit { preferences ->
+            preferences[preferencesKey] = value
+        }
+    }
+
+    override suspend fun getString(key: String): String? {
+        return try {
+            val preferencesKey = stringPreferencesKey(key)
+            val preferences = context.dataStore.data.first()
+            preferences[preferencesKey]
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    override suspend fun putInt(key: String, value: Int) {
+        val preferencesKey = intPreferencesKey(key)
+        context.dataStore.edit { preferences ->
+            preferences[preferencesKey] = value
+        }
+    }
+
+    override suspend fun getInt(key: String): Int? {
+        return try {
+            val preferencesKey = intPreferencesKey(key)
+            val preferences = context.dataStore.data.first()
+            preferences[preferencesKey]
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+}
--- app/src/main/java/ir/reversedev/documentbag/utils/Constants.kt
+++ app/src/main/java/ir/reversedev/documentbag/utils/Constants.kt
@@ -5,4 +5,6 @@ object Constants {
     const val ENGLISH_LANG = "en"
     const val PERSIAN_LANG = "fa"
 
+    const val DATA_STORE_NAME = "DOCUMENT_BAG_DATA_STORE_NAME"
+
 }
